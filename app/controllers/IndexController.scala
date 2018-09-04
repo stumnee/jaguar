0b7202a42bab38968b0bc9c24972374c37b40cfe
Saran Tumnee <stumnee@gmail.com>
2018-09-03 22:32:54 -0700

    websocket validation of user session

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -7,6 +7,8 @@ import akka.actor.ActorSystem
 import play.api.libs.streams.ActorFlow
 import play.api.mvc._
 
+import scala.concurrent.Future
+
 class IndexController @Inject()(
       cc: ControllerComponents)(
       implicit val system: ActorSystem,
@@ -23,9 +25,25 @@ class IndexController @Inject()(
     }
   }
 
-  def ws = WebSocket.accept[String, String] { request =>
-    ActorFlow.actorRef { out =>
-      MyWebSocketActor.props(out)
-    }
+  def testWebsocket() = Action { request =>
+    Ok(views.html.testWebsocket())
   }
+
+  def ws = WebSocket.acceptOrResult[String, String] { request =>
+    Future.successful(request.session.get("user") match {
+      case None => Left(Forbidden("Must log in first"))
+      case Some(_) => Right(
+        ActorFlow.actorRef { out =>
+          MyWebSocketActor.props(out)
+        }
+      )
+    })
+
+  }
+//
+//  def ws = WebSocket.accept[String, String] { request =>
+//    ActorFlow.actorRef { out =>
+//      MyWebSocketActor.props(out)
+//    }
+//  }
 }
